a458ab8db1ab05688da36278246f048b2e34f29d
Diego Nascimento <diego.nascimento@gsw.com.br>
2024-05-22 12:12:10 -0300

    feat: Criação da interface de repository

---
+++ src/main/java/com/malyah/ToDoList/domain/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.malyah.ToDoList.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.malyah.ToDoList.domain.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{ 
+
+}
